fdef06d52555c6944289efa46889a552f96ad0fb
YunHyeong <150113466+newblackrabbit@users.noreply.github.com>
2024-08-02 15:06:29 +0900

    [Bronze IV] Title: 상근날드, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.py
@@ -0,0 +1,11 @@
+ham = []
+coke = []
+
+ham.append(int(input()))
+ham.append(int(input()))
+ham.append(int(input()))
+coke.append(int(input()))
+coke.append(int(input()))
+
+print(min(ham)+min(coke)-50)
+
