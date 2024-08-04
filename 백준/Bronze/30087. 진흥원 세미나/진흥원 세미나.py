c61ffae39cda7dcac1e70b9d2e632e5a95c8fcf5
YunHyeong <150113466+newblackrabbit@users.noreply.github.com>
2024-08-04 16:54:15 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,8 @@
+classes = ['Algorithm','DataAnalysis','ArtificialIntelligence','CyberSecurity','Network','Startup','TestStrategy']
+
+num = [204,207,302,'B101',303,501,105]
+
+n = int(input())
+
+for i in range(n):
+    print(num[classes.index(input())])
